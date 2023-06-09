97da52facf8a486425d9ac6c2e4102c298301977
Pratyaksh-bit <60149337+Pratyaksh-bit@users.noreply.github.com>
2023-06-09 19:56:15 +0530

    Update script.js

--- Wedding-Invitation-master/js/script.js
+++ Wedding-Invitation-master/js/script.js
@@ -1,6 +1,4 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
- */
+
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
@@ -26,7 +24,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Jun 23, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
